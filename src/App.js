6b2a6f7f940e01e0dd50a3b9c005fade9e451edc
AhmedAbderrahmanElBaghdadi04 <baghdadiahmed004@gmail.com>
2023-10-25 10:50:44 +0200

    'app'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './ap1.css';
+import '/Calcul1';
+import Calcul1 from '/Calcul1';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Calcul1></Calcul1>
     </div>
   );
 }
