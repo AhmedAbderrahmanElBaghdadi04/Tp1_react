4721e223c701e44603c89541a02a30a1e95dd0d0
AhmedAbderrahmanElBaghdadi04 <148655673+AhmedAbderrahmanElBaghdadi04@users.noreply.github.com>
2023-11-11 16:36:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Calculatrice from '../src/tp1ccl/Calculatrice1';
 function App() {
   return (
     <div className="App">
-   <Calculatrice></Calculatrice>
+   <Calculatrice />
     </div>
   );
 }
